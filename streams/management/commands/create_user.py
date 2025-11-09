7880775965fcd15d735e973f7bdc2ff000954c0c
Alexander Wagner <github@alexanderwagnerdev.com>
2025-11-09 18:37:52 +0100

    Update create_user.py

--- streams/management/commands/create_user.py
+++ streams/management/commands/create_user.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 import os
 
 class Command(BaseCommand):
-    help = "Erstellt User basierend auf ENV"
+    help = "Create User from ENV"
 
     def handle(self, *args, **kwargs):
         username = os.getenv("WEB_USERNAME", "admin")
         password = os.getenv("WEB_PASSWORD", "password")
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email="", password=password)
-            self.stdout.write(self.style.SUCCESS(f"User '{username}' wurde erstellt."))
+            self.stdout.write(self.style.SUCCESS(f"User '{username}' was created."))
         else:
-            self.stdout.write(f"User '{username}' existiert bereits.")
+            self.stdout.write(f"User '{username}' already exists.")
